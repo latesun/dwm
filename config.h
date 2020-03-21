612aa4f1aeee70e830defe4bc77174bc8a16e66c
latesun <latesun.lee@bindo.com>
2020-03-21 14:18:15 +0800

    #1. modify font; #2. modify browser

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font:size=20" };
-static const char dmenufont[]       = "Source Pro Code:size=20";
+static const char *fonts[]          = { "MesloLGS Nerd Font:size=20" };
+static const char dmenufont[]       = "MesloLGS Nerd Font:size=20";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[]  = { "chromium", NULL };
+static const char *browsercmd[]  = { "google-chrome", "--force-device-scale-factor=1.5", NULL };
 static const char *downvol[]   = { "amixer", "set", "-q", "Master", "5%-", "unmute", NULL };
 static const char *upvol[]   = { "amixer", "set", "-q", "Master", "5%+", "unmute", NULL };
 static const char *mutevol[]   = { "amixer", "set", "-q", "Master", "toggle", NULL };
